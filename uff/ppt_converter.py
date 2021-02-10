346c6f10d466c107591d0c3695c157e2d5420cb5
Robin De Baets <robindebaetsdev@gmail.com>
2021-02-10 13:02:51 +0100

    Remove exception handling in ppt converter

--- uff/ppt_converter.py
+++ uff/ppt_converter.py
@@ -5,8 +5,6 @@ def convert_to_pdf(ppt_input, pdf_output):
     try:
         subprocess.run(["unoconv", "-o", pdf_output, ppt_input], stdout=subprocess.DEVNULL,
                    stderr=subprocess.DEVNULL)
-    # except FileNotFoundError:
-    #     # If unoconv is not installed, just ignore it
-    #     pass
-    except Exception as e:
-        print(e)
+    except FileNotFoundError:
+        # If unoconv is not installed, just ignore it
+        pass
